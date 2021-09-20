3e933dc39ff25529ef8d99c835b91b1f73010461
Phillipos Admasu <padmasu@ymail.com>
2021-09-19 21:58:28 -0600

    Updating to correct version of .py file

--- music_script.py
+++ music_script.py
@@ -1,1 +1,16 @@
-print("hi")
+import pandas as pd 
+import numpy
+import csv
+
+df = pd.read_csv(r"C:\Users\padma\Documents\Hot-100-Audio-Features.csv")
+#print(df)
+temp =  df['tempo']
+
+temp_null = pd.isnull(temp)
+#print(temp_null)
+
+df2 = df[['spotify_genre','tempo']]
+#print(df2)
+
+temp1 = df2['tempo'].mode()[0] 
+print(temp1)
